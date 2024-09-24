2f0f276ba4779505ec17a53d343b126cf8f2c439
bwissal13 <baazizwissal13@gmail.com>
2024-09-24 15:10:26 +0100

    update DatabaseConfig

--- src/main/bati/config/DatabaseConfig.java
+++ src/main/bati/config/DatabaseConfig.java
@@ -13,26 +13,23 @@ public class DatabaseConfig {
     private String username;
     private String password;
 
-    // Constructeur privé pour empêcher l'instanciation directe
+
     private DatabaseConfig() {
         try {
-            // Chargement des propriétés de configuration
+
             Properties props = new Properties();
             props.load(getClass().getClassLoader().getResourceAsStream("database.properties"));
 
-            // Récupération des informations de connexion à partir du fichier de propriétés
             this.url = props.getProperty("db.url");
             this.username = props.getProperty("db.username");
             this.password = props.getProperty("db.password");
 
-            // Création de la connexion à la base de données
             this.connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException | NullPointerException | IOException e) {
-            e.printStackTrace();  // Gestion de l'erreur si la connexion échoue
+            e.printStackTrace();
         }
     }
 
-    // Méthode publique pour obtenir l'instance unique de DatabaseConfig
     public static synchronized DatabaseConfig getInstance() {
         if (instance == null) {
             instance = new DatabaseConfig();
@@ -40,7 +37,6 @@ public class DatabaseConfig {
         return instance;
     }
 
-    // Méthode pour obtenir la connexion à la base de données
     public Connection getConnection() {
         return connection;
     }
